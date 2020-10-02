69d64883c323b4c8a7e015db14a1deca3c9d8714
Vishal Tyagi <vishal.tyagi@ubisoft.com>
2020-10-02 19:20:46 +0530

    finished algorithm

--- script.py
+++ script.py
@@ -10,7 +10,29 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+def backtrackSolve(bo):
+    find =  find_unsolved(bo)
+    if not find:
+        return True
+    else:
+        row, column = find
+    
+    for i in range (1,10):
+        if check_validity(bo,i,(row,column)):
+            bo[row][column] = i
+
+            if backtrackSolve(bo):
+                return True
+
+            bo[row][column] = 0
+    
+    return False
+
+
+
+
 def check_validity(bo,number,position):
+    
     #Checking the row
     for i in range (len(bo[0])):
         if bo[position[0]][i] == number and  position[1] != i:
@@ -19,6 +41,17 @@ def check_validity(bo,number,position):
     for i in range (len(bo)):
         if bo[i] [position[1]] == number and position [0] != i:
             return False
+    
+    # checking 3 x 3 box
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+
+    for i in range (box_y * 3, box_y*3 + 3):
+        for j in range (box_x * 3, box_x*3 + 3):
+            if bo[i][j] == number and (i,j) != position:
+                return False
+   
+    return True
 
 #Function to print the board
 
@@ -46,3 +79,10 @@ def find_unsolved(bo):
             if bo[i][j]==0:
                 return (i,j) # returning array number with unsolved piece
 
+    return None
+
+
+print_board(board)
+backtrackSolve(board)
+print("\n")
+print_board(board)
